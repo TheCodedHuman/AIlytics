890c30f9c42557af93f0bcde296f5b04d0a16271
TheCodedHuman <mokshmahawar05@gmail.com>
2025-06-08 19:14:20 +0530

    added static repo data

---
+++ code/src/utils/repoData.js
@@ -0,0 +1,24 @@
+const repoData = [
+    { name: "Python", views: 10003849 },
+    { name: "JavaScript", views: 8950560 },
+    { name: "MySQL", views: 7541256 },
+    { name: "CSS", views: 6189432 },
+    { name: "HTML", views: 5005487 },
+    { name: "TailwindCSS", views: 3200493 },
+    { name: "React.js", views: 1200039 },
+    { name: "Node.js", views: 950000 },
+    { name: "Django", views: 870000 },
+    { name: "Flask", views: 760000 },
+    { name: "Ruby on Rails", views: 680000 },
+    { name: "Angular", views: 590000 },
+    { name: "Vue.js", views: 520000 },
+    { name: "Bootstrap", views: 450000 },
+    { name: "Sass", views: 380000 },
+    { name: "GraphQL", views: 310000 },
+    { name: "TypeScript", views: 250000 },
+    { name: "Kotlin", views: 200000 },
+    { name: "Swift", views: 150000 },
+    { name: "Go", views: 100000 },
+];
+
+export default repoData;
